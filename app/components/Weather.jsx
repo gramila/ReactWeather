e5d3cc0e438741d73190b03e83ae74692bf6df64
Gema Beltran <gramila65@gmail.com>
2017-02-05 15:49:02 -0800

    Add custom styles support

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -36,7 +36,7 @@ var Weather = React.createClass({
 
         function renderMessage(){
             if(isLoading){
-                return <h3 className="text-centered">Fetcing weather..</h3>
+                return <h3 className="text-center">Fetcing weather..</h3>
             } else if(temp && location){
                 return <WeatherMessage temp={temp} location={location}/>
             }
@@ -52,7 +52,7 @@ var Weather = React.createClass({
 
         return (
             <div>
-                <h1 className="text-centered">Get Weather</h1>
+                <h1 className="text-center page-title">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch} />
                 {renderMessage()}
                 {renderError()}
